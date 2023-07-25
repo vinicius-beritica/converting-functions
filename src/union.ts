b27174c9b87b8ae383a3049af90d137e68c75959
beritica <vinicius.beritica@gmail.com>
2023-07-25 14:08:34 -0300

    Implementado o código de union

--- src/union.ts
+++ src/union.ts
@@ -9,5 +9,20 @@
  * @returns somente os itens definidos.
  */
 export const union = <T>(...args: T[]): T[] => {
-  return null;
+  const arrUnion = [];
+  const param = args as unknown as ArrayLike<T>[];
+  for (let i = 0; param.length > i; i++) {
+    for (let j = 0; param[i].length > j; j++) {
+      let exists = false;
+      for (let k = 0; arrUnion.length > k; k++) {
+        if (param[i][j] === arrUnion[k]) {
+          exists = true;
+        }
+      }
+      if (!exists) {
+        arrUnion.push(param[i][j]);
+      }
+    }
+  }
+  return arrUnion;
 };
