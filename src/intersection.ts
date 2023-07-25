edb7ccef7232ffd8308b594060dbb89b5fbe9c6c
beritica <vinicius.beritica@gmail.com>
2023-07-25 13:37:02 -0300

    Implementado o código de intersection

--- src/intersection.ts
+++ src/intersection.ts
@@ -8,5 +8,21 @@
  * @returns somente os itens definidos.
  */
 export const intersection = (left: number[], right: number[]): number[] => {
-  return null;
+  const arrIntersec = [];
+  for (let i = 0; left.length > i; i++) {
+    for (let j = 0; right.length > j; j++) {
+      if (right[j] === left[i]) {
+        let exists = false;
+        for (let k = 0; arrIntersec.length > k; k++) {
+          if (left[i] === arrIntersec[k]) {
+            exists = true;
+          }
+        }
+        if (!exists) {
+          arrIntersec.push(left[i]);
+        }
+      }
+    }
+  }
+  return arrIntersec;
 };
