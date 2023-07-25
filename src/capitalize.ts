94ddd05f6dac2dd95565782303540d60595b230f
beritica <vinicius.beritica@gmail.com>
2023-07-25 12:06:53 -0300

    Implementando código para capitalize

--- src/capitalize.ts
+++ src/capitalize.ts
@@ -5,5 +5,7 @@
  * @returns o texto enviado por parâmetro, alterado para capitalize.
  */
 export const capitalize = (text: string): string => {
-  return '';
+  const strFirstUpper: string =
+    text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+  return strFirstUpper;
 };
