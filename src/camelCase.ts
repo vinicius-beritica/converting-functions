91e3fb50e340e63f5ddb3bd9c6bbc3a11bcbac8a
beritica <vinicius.beritica@gmail.com>
2023-07-25 11:59:28 -0300

    Implementando código para camelCase

--- src/camelCase.ts
+++ src/camelCase.ts
@@ -5,5 +5,18 @@
  * @returns o texto enviado por parâmetro, alterado para camelCase.
  */
 export const camelCase = (word: string): string => {
-  return '';
+  const strCamelCase: string[] = word.split('');
+
+  for (let i: number = 0; strCamelCase.length > i; i++) {
+    strCamelCase[0] = strCamelCase[0].toLowerCase();
+    if (
+      strCamelCase[i] == ' ' ||
+      strCamelCase[i] == '-' ||
+      strCamelCase[i] == '_'
+    ) {
+      strCamelCase[i] = '';
+      strCamelCase[i + 1] = strCamelCase[i + 1].toUpperCase();
+    }
+  }
+  return strCamelCase.join('');
 };
