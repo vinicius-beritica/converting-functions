2cb6f6c253fd4004aafe572087e3dd7ecb70de9a
beritica <vinicius.beritica@gmail.com>
2023-07-25 13:46:58 -0300

    Implementado o código de pascalCase

--- src/pascalCase.ts
+++ src/pascalCase.ts
@@ -5,5 +5,14 @@
  * @returns o texto enviado por parâmetro, alterado para pascalCase.
  */
 export const pascalCase = (word: string): string => {
-  return '';
+  let arrayStr: string[] = word.split('');
+
+  for (let i: number = 0; arrayStr.length > i; i++) {
+    arrayStr[0] = arrayStr[0].toUpperCase();
+    if (arrayStr[i] === ' ' || arrayStr[i] === '-' || arrayStr[i] === '_') {
+      arrayStr[i] = '';
+      arrayStr[i + 1] = arrayStr[i + 1].toUpperCase();
+    }
+  }
+  return arrayStr.join('');
 };
