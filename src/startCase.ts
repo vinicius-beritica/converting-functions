c0b0e5c5d1ea42f7df23cf1c597c8ef2b7e0c49c
beritica <vinicius.beritica@gmail.com>
2023-07-25 14:04:13 -0300

    Implementado o código de startCase

--- src/startCase.ts
+++ src/startCase.ts
@@ -5,5 +5,18 @@
  * @returns o texto enviado por parâmetro, alterado para startCase.
  */
 export const startCase = (word: string): string => {
-  return '';
+  const strStarCase: string[] = word.split('');
+
+  for (let i: number = 0; strStarCase.length > i; i++) {
+    strStarCase[0] = strStarCase[0].toUpperCase();
+    if (
+      strStarCase[i] === '-' ||
+      strStarCase[i] === '_' ||
+      strStarCase[i] === ' '
+    ) {
+      strStarCase[i] = ' ';
+      strStarCase[i + 1] = strStarCase[i + 1].toUpperCase();
+    }
+  }
+  return strStarCase.join('');
 };
